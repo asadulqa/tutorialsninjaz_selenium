9270482668fb62d283c2ad516c4c0b301ad6672e
Asadul Haque <112169569+asadulqa@users.noreply.github.com>
2025-04-16 11:03:39 +0600

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -2,13 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def before_scenario(context, scenario):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-background-timer-throttling")
+    chrome_options.add_argument("--disable-backgrounding-occluded-windows")
+
 
     context.driver = webdriver.Chrome(options=chrome_options)
 
 def after_scenario(context, scenario):
     if hasattr(context, 'driver'):
-        context.driver.quit()
+        context. driver.quit()
